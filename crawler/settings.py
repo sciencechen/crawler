61588b5993d4c4a76a38ae3031ae64d3a58f9e9a
sciencechen <13790922187@139.com>
2021-06-13 13:32:10 +0800

    修改了请求头User-Agent，尝试能否解决反爬问题

--- crawler/settings.py
+++ crawler/settings.py
@@ -22,7 +22,7 @@ MYSQL_PASSWORD = '1234'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
